2fcc2326586244aafce3a95d06f94d4f30a8763b
JhonatanGP <jguzman5292@ceuandalucia.es>
2023-12-01 20:29:25 +0100

    Soy Jhonatan y me encantan los polvorones

---
+++ ProyectoExEj2/src/Polvoron.java
@@ -0,0 +1,9 @@
+
+public class Polvoron {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Soy Jhonatan y me encantan los polvorones");
+	}
+
+}
