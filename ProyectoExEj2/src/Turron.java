054d45567033a13ce3c5f8d9f1893cfb8a212680
JhonatanGP <jguzman5292@ceuandalucia.es>
2023-12-01 20:39:46 +0100

    Pero mi preferido es el de chocolate

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -3,7 +3,7 @@ public class Turron {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Soy Jhonatan y me encantan los turrones");
+		System.out.println("Soy Jhonatan y me encantan los turrones, pero mi preferido es el de chocolate.");
 
 	}
 
